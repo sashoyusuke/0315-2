f9478d40de3052648f5a8cbf9e153e9d14d2b7c7
sashoyusuke <zyesi731@gmail.com>
2021-03-15 09:04:23 +0900

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <input type="text" v-model="text" >
+  <button @click="getaddress()">住所自動入力</button>
+  <p>Address:{{address}}</p>
+  </div>
+</template>
+
+<script>
+
+export default{
+data(){
+  return{
+    text:""
+  };
+},
+methods:{
+
+}
+}
